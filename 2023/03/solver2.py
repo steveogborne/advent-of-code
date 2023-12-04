57c1b3d98945388c0a117b6b78bc1e58d1d0dba9
Steve Ogborne <steve.ogborne@gmail.com>
2023-12-04 22:08:40 +0000

    Touch numbers now is simpler and returns a prototype of teh power coordinate. Poss_gears is now gear_search and returns gear coords and power ratio lists

--- 2023/03/solver2.py
+++ 2023/03/solver2.py
@@ -16,70 +16,83 @@ for i in range(3):
 import re
 from string import digits
 
-# Step 1 locate * coordinates
-# Haha! Can half resuse unused code. Procrastination win!
-# Step 2 build in logic to test if * is gear
-# Steb 2B modify touch function for symbols?
+# Step 1 modify touch function for symbols
 # Create touch_number function by modifying touch_symbol function to help with logic in poss_gear_cords
 # Touch number will take one signle coordinate (phew! simpler) and check for TWO numbers
 
 def touch_numbers(coordinate, schematic):
     touching = False # unless shown otherwise
     r = coordinate[0] # row coordinate for search +/-1
-    c = coordinate[0] # columns coordinates for search +/-1
+    c = coordinate[1] # columns coordinates for search +/-1
 
     # set lower and upper bounds of search box to take into account edges
-    if r == 0: r_min = 0
-    else: r_min = r-1
-    # print("R_min is: "+str(r_min))
-    if r == len(schematic)-1: r_max = len(schematic)-1
-    else: r_max = r+1
-    # print("R_max is: "+str(r_max))
-    if c == 0: c_min = 0
-    else: c_min = c-1
-    # print("C_min is: "+str(c_min))
-    if c == [len(schematic[0])-1]: c_max = len(schematic[0]-1)
-    else: c_max = c+1
-    # print("C_max is: "+str(c_max))
+    r_min = r-1
+    r_max = r+1
+    c_min = c-1
+    c_max = c+1
 
-    # Create a list of characters surrounding the number "search_box"
-    rowA = schematic[r_min][c_min:c_max+1] # row of characters above number (or through if top)
-    rowB = schematic[r][c_min:c_max+1] # row of characters though number
-    rowC = schematic[r_max][c_min:c_max+1] # row of characters under number (or through if bottom)
-    search_box = rowA + rowB + rowC
-    # print(search_box)
+    # Create a list of coordinates surrounding the gear
+    # To avoid duplicates from same number only search in the bounding columns.
+    # To avoid duplicates from repeating lines at edges change logic to only include bounding box elements if valid
+    # This way max count = 2 and gear when count = 2
 
-    #inspect search box to see if there >= two adjacent number
+    search_coords = []
+    if r != 0 and c != 0: search_coords.append([r_min,c_min]) # top left
+    if r != 0 and c != len(schematic[0])-1: search_coords.append([r_min,c_max]) # top right
+    if c != 0: search_coords.append([r,c_min]) # mid left
+    if c != len(schematic[0])-1: search_coords.append([r,c_max]) # mid right
+    if r != len(schematic)-1 and c != 0: search_coords.append([r_max,c_min]) # bottom left
+    if r != len(schematic)-1 and c != len(schematic[0])-1: search_coords.append([r_max,c_max]) # bottom right
+
+    print(search_coords)
+    # Inspect coordinates to see if there are two adjacent numbers
     count = 0
-    for char in search_box:
-        if char.isdecimal():
+    number_coords = []
+    for char in search_coords:
+        row = char[0]
+        column = char[1]
+        symbol = schematic[row][column]
+        if symbol.isdecimal():
+            number_coords.append([row,column])
             count += 1
         else:
             continue
+    print(number_coords)
 
+    power_ratio = 1 # initialise
     # Return logic depending
-    if count >= 2: return(True)
-    else: return(False)
+    if count == 2: return(True, power_ratio)
+    else: return(False, 0)
 
-def gear_coords(schematic):
-    symbol_coords = [] # empty list to fill with coordinates
+# Step 2 locate * coordinates
+# Haha! Can half resuse unused code. Procrastination win!
+# Step 2B build in logic to test if * is gear
+
+def gear_search(schematic):
+    gear_coords = [] # empty list to fill with coordinates
+    power_ratios = [] # empty list to fill with power ratios
     for line_coord, line in enumerate(schematic):
         for char_coord, char in enumerate(line):
             if char == "*":
-                if touch_numbers([line_coord, char_coord], schematic):
+                gear = touch_numbers([line_coord, char_coord], schematic)
+                print(gear)
+                if gear[0] == True:
                     print("Gear found at: "+str(line_coord)+", "+str(char_coord)) # test
-                    symbol_coords.append([line_coord, char_coord])
+                    power_ratios.append(gear[1])
+                    gear_coords.append([line_coord, char_coord])
                 else:
                     print("Star found at: "+str(line_coord)+", "+str(char_coord)+" but not a gear") # test
                     continue
-    return(symbol_coords)
+    return(gear_coords, power_ratios)
 
-poss_gears = gear_coords(snippet) # test
-print(poss_gears)
+gears = gear_search(snippet) # test
+print(gears)
 
-# Step 2A List all numbers with their coordinates.
-# number object has data structure [value, row_index, column_indexes]
-# solumn_indexes will have multiple values according to digits in the number
+# Step 3 Identify numbers
+# Reference gear coordinate
+# Can I use coordinates in extracted numbers to cross reference against coordinates in touch_numbers?
+# Brute force it with fresh search?
+
 
 def extract_numbers(schematic):
     numbers = [] # initialise
