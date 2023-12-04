493601a9808c272594fa7093f2b08108782fe8d3
Steve Ogborne <steve.ogborne@gmail.com>
2023-12-04 15:11:39 +0000

    Wrote a function to strip schematic of all . and digits, just to confirm what symbols present.

--- 2023/03/solver.py
+++ 2023/03/solver.py
@@ -10,5 +10,25 @@ input = open("puzzle_input.txt")
 schematic = input.readlines()
 input.close()
 
-#test
-print(schematic[0])
+symbols = ['+', '-', '=', '/', '*', '#', '$', '%', '@', '&']
+
+import re
+from string import digits
+
+def reduce_to_symbol(thing):
+    reduced_schematic = []
+    remove_digits = str.maketrans('', '', digits)
+    for line in thing:
+        new_line = line.replace(".","")
+        new_line = new_line.translate(remove_digits)
+        reduced_schematic.append(new_line)
+    return(reduced_schematic)
+
+snippet = reduce_to_symbol(schematic)
+snippet_out = ""
+for i in snippet:
+    snippet_out += i -"/n"
+print(snippet_out)
+
+# test_line = schematic[0]
+# line_coordinate = index
