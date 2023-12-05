da6098357f243fa672000616c3efb1fb2c939324
Steve Ogborne <steve.ogborne@gmail.com>
2023-12-05 13:49:06 +0000

    Modified the function that cleans the data to include game ID and made it neater. Modified find_matches according to new data structure, created a new list containing card IDs, match results and an empty space to update copies

--- 2023/04/solver2.py
+++ 2023/04/solver2.py
@@ -7,46 +7,46 @@
 
 # Step 0 readfile
 with open("puzzle_input.txt") as file:
-    all_cards = file.read().splitlines()
+    raw_cards = file.read().splitlines()
 
-# print(all_cards[0]) # test
+# print(raw_cards[0]) # test
 
 # Step 1 define function to split line into two lists, winning numbers and your numbers - reuse
 
-def extract_numbers(line):
-    remove_head = line.split(":")[1] # Removes leading "Card x:"
-    numbers = [remove_head.split("|")[0].strip().split(" "), remove_head.split("|")[1].strip().split(" ")] # split, cleanup and split
-    winning_numbers = [int(x) for x in numbers[0] if x] # removes empty elements and turns strings into integers
-    chosen_numbers = [int(x) for x in numbers[1] if x]
+def cleaned_card(line):
+    card = [int(line.split(":")[0].split(" ")[-1]),                                            # card number
+               [int(x) for x in line.split(":")[1].split("|")[0].strip().split(" ") if x],     # winning numbers
+               [int(x) for x in line.split(":")[1].split("|")[1].strip().split(" ") if x]]     # chosen numbers
+    # Split game/numbers, split winning/chosen, cleanup and split strings into lists
+    # ... with empty elements removed and strings turned into integers
 
-    return(winning_numbers,chosen_numbers)
+    # Return "card" which has format: ["card_ID", ["list of winning numbers"], ["list of chosen numbers"]]
+    return(card)
 
-# print(extract_numbers(all_cards[0])) # test
+# print(cleaned_card(raw_cards[0])) # test
 
 # Step 2 define function to calculate matches for a given winning number and chosen number list - reuse
 
-def find_matches(numbers):
+def find_matches(card):
     matches = 0
-    for x in numbers[0]:
-        if x in numbers[1]:
+    for x in card[1]:
+        if x in card[2]:
             matches += 1
     return(matches)
 
-# Step 3 Create a score function to return score for x number of matches - remove
-def score_matches(matches):
-    scores = [0, 1] # 0 matches returns 0 score
-    for x in range(9): scores.append(scores[-1]*2) # 10 matches is maximum but there would be no harm in adding more
-    return(scores[matches])
-
-# print(score_matches(3)) # test
+print(find_matches(cleaned_card(raw_cards[0]))) # test
 
 # NEW - write a function that creates a list of cards, matches, copies. Iterate over the list to increment copies according to preceeding matches
+# tracker = [[game(card), find_matches(card), 0] for card in all_cards]
+
+tracker = [(cleaned_card(x)[0], find_matches(cleaned_card(x)), 0) for x in raw_cards]
+print(tracker[0])
 
 # Step 4 iterate over all_cards to count matches - modify to count cards
 
-total_score = 0
-for card in all_cards:
-    score = score_matches(find_matches(extract_numbers(card)))
-    total_score += score
+# total_score = 0
+# for card in all_cards:
+#     score = score_matches(find_matches(extract_numbers(card)))
+#     total_score += score
 
-print(total_score)
+# print(total_score)
