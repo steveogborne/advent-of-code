20f5f9104dc7500497ccbd6884016e2cfe27ab05
Steve Ogborne <steve.ogborne@gmail.com>
2023-12-05 23:34:16 +0000

    Adjusted find_closest to work witha  seed range, keep track of minimum and return the minimum over the whole range. However it is slow! Added some terminal commands to visualise progress for ONE seed range and each seed range would take about half an hour so the process needs to be heavily optimised. Next I'm going to find a way to see if navigate_map would be the same as last time to reduce the amount of times it neext to be called

--- 2023/05/solver2.py
+++ 2023/05/solver2.py
@@ -3,6 +3,8 @@
 # Expand the search to find the nearest location for all of these seeds
 # Should be able to reuse a lot of code this time. The only problem will be if if the data becomes too unweildy
 
+import os
+
 # Need a function to create maps. Map returned as a dictionary. For part 2 stays the same
 def create_map(raw_map):
     # raw_map.split(":")[0].split(" ")[0].split("-") # "input,to,output"
@@ -77,13 +79,43 @@ def translate_seed(seed, maps):
     else: location = "Error"
     return(location)
 
+# Function to find the closest location given a seed, range pair and maps
+def find_closest(seed, seed_range, maps):
+    closest = 0
+    current_seed = seed
+    for n in range(seed_range):
+        if n%100000 ==0:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            calculations_remaining = seed_range - n
+            print(calculations_remaining, "calculations remaining")
+        location = translate_seed(current_seed, maps)
+        if location < closest or closest == 0:
+            closest = location
+        else: continue
+    return(closest)
+
+
 # Solve the problem! - This time rather than calculating a list of seed_locatiosn and using a min function,
 # we should keep track of the minimum and compare so that we can compute one seed at a time
 def main():
-    seeds = process_raw("puzzle_input.txt")[0] # list of seeds
-    # maps = process_raw("puzzle_input.txt")[1] # list of map dictionaries
+    seeds = process_raw("puzzle_input.txt")[0] # list of seed pairs
+    maps = process_raw("puzzle_input.txt")[1] # list of map dictionaries
     # test_dest = translate_seed(seeds[0], maps)
     # print(test_dest)
+
+    # all_seed_minimum = 0
+    min_location = find_closest(seeds[1][0], seeds[1][1], maps)
+    # for seed in seeds:
+    #     min_location = translate_seed(current_seed, maps)
+    #     if location < closest or closest == 0:
+    #         closest = location
+    #     else: continue
+    # return(closest)
+
+    print(min_location)
+    # keep track of minimum
+    # create seed iterator for given seed, range pair
+    # then repeat iterator for each seed pair
 
     # seed_locations = [[seed for seed in seeds], [translate_seed(seed, maps) for seed in seeds]] # need to define translate_seeds function
     # for x in range(len(seed_locations[0])): print(seed_locations[0][x], seed_locations[1][x]) # check do the seeds change?
