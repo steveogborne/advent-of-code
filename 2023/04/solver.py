7165ddd422dd90c7e16466e0fd89f996974afe34
Steve Ogborne <steve.ogborne@gmail.com>
2023-12-05 12:24:29 +0000

    Added function to take a line formatted according to the puzzle into and turn it into two lists for the winngin and chosen numbers, wrote a function to return score for number matches. Learning about list comprehension and trying to be more pythonic...

--- 2023/04/solver.py
+++ 2023/04/solver.py
@@ -4,8 +4,27 @@
 # Score the game based on doubling your score every winning number
 
 # Step 0 readfile
-
 with open("puzzle_input.txt") as file:
     all_cards = file.read().splitlines()
 
-print(all_cards[0])
+# print(all_cards[0]) # test
+
+# Step 1 define function to split line into two lists, winning numbers and your numbers
+
+def extract_numbers(line):
+    remove_head = line.split(":")[1] # Removes leading "Card x:"
+    numbers = [remove_head.split("|")[0].strip().split(" "), remove_head.split("|")[1].strip().split(" ")] # split, cleanup and split
+    winning_numbers = [int(x) for x in numbers[0] if x] # removes empty elements and turns strings into integers
+    your_numbers = [int(x) for x in numbers[1] if x]
+
+    return(winning_numbers,your_numbers)
+
+print(extract_numbers(all_cards[0])) # test
+
+# Create a score function to return score for x number of matches
+def score_matches(matches):
+    scores = [0, 1] # 0 matches returns 0 score
+    for x in range(9): scores.append(scores[-1]*2) # 10 matches is maximum but there would be no harm in adding more
+    return(scores[matches])
+
+# print(score_matches(3)) # test
