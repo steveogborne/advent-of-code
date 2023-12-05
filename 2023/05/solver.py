6aeecd6dce161792ead90237bb3af80a0d71bf7c
Steve Ogborne <steve.ogborne@gmail.com>
2023-12-05 15:28:52 +0000

    New directory, puzzle input, solver created and sketched out puzzle solution

---
+++ 2023/05/solver.py
@@ -0,0 +1,8 @@
+# We have a list of seeds and a set of maps
+# Extract the seed list and maps
+# Write a function to use a map to convert from source to destination
+# Write a function to use all maps to convert from seed to location
+# Write a function to select the nearest (lowest) location
+
+with open("puzzle_input.txt") as file:
+    raw_file = file
