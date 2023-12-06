6e5f59a34a533d203555f44f11cf044476fd5cdd
Steve Ogborne <steve.ogborne@gmail.com>
2023-12-06 18:35:09 +0000

    Easy! Wrote the four functions defined in my sketch and they worked first time. On to part 2

--- 2023/solver.py
+++ 2023/solver.py
@@ -18,10 +18,40 @@ distances = [int(i) for i in distances if i]
 races = [[times[x], distances[x]] for x in range(len(times))]
 print(races)
 
+# races has structure [[race_time, distance_record], [].. ] for 4 races
+
 # function to calculate distance travelled for charge time, race time
+def calc_dist(charge_time,race_time):
+    travel_time = race_time - charge_time
+    distance = charge_time*travel_time
+    return(distance)
+
+# print(calc_dist(2,races[0][0])) # test
 
 # function to compare if distance beats record
+def is_win(distance, race_record):
+    if distance > race_record: return(True)
+    else: return(False)
+
+# print(is_win(calc_dist(2,races[0][0]), races[0][1])) # test
 
 # function to iterate over charge time and count wins
+def count_wins(race):
+    race_time = race[0]
+    race_record = race[1]
+    win_count = 0
+    for x in range(race_time):
+        if(is_win(calc_dist(x, race_time),race_record)):
+            win_count += 1
+    return(win_count)
+
+# print(count_wins(races[0])) # test
 
 # function to iterate over races and return product of wins
+def score_races(races):
+    score = 1
+    for race in races:
+        score *= count_wins(race)
+    return(score)
+
+print(score_races(races)) # result
