2db365546ef40d36884e54ffa3fabf5b95c87cf5
Steve Ogborne <steve.ogborne@gmail.com>
2023-12-06 17:59:09 +0000

    New puzzle directory, input and solver created. Problem broken down in solver

---
+++ 2023/solver.py
@@ -0,0 +1,8 @@
+# Boat races
+# Each race is allocated x ms to run
+# Boat speed charges 1 mm/s per ms
+# Boat speed is constant and travels for y ms where y is x - charge time
+# Distance = boat speed * y
+# For each race you need to beat the record from puzzle_input
+# For each race determine the number of ways to beat the record = z
+# Final answer is product of z
