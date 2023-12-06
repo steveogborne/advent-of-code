891ec86bd5365a45e07bd691990039d82528c50b
Steve Ogborne <steve.ogborne@gmail.com>
2023-12-06 18:20:18 +0000

    Added code to read abd parse input data and sketched out solution

--- 2023/solver.py
+++ 2023/solver.py
@@ -6,3 +6,22 @@
 # For each race you need to beat the record from puzzle_input
 # For each race determine the number of ways to beat the record = z
 # Final answer is product of z
+
+# Extract race data
+with open("puzzle_input.txt") as file:
+    temp = file.read().splitlines()
+
+times = temp[0].split(":")[1].strip().split(" ")
+times = [int(i) for i in times if i]
+distances = temp[1].split(":")[1].strip().split(" ")
+distances = [int(i) for i in distances if i]
+races = [[times[x], distances[x]] for x in range(len(times))]
+print(races)
+
+# function to calculate distance travelled for charge time, race time
+
+# function to compare if distance beats record
+
+# function to iterate over charge time and count wins
+
+# function to iterate over races and return product of wins
