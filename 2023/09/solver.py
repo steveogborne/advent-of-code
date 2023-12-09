3f149ac586bf27323144937c36e49a35ff33f8bd
Steve Ogborne <steve.ogborne@gmail.com>
2023-12-09 22:17:12 +0000

    Updated bash script to create new directory based on old directory list. Copied code from internet... It works though. Created a new dir and solver for todays puzzle (and tomorrow by accident). Todays problem scoped out and program half working. It creates a list of lists where each subsequent sublist is the differences list of the previous one. Now need to add the summing logic to find the predicted value.

---
+++ 2023/09/solver.py
@@ -0,0 +1,35 @@
+# Problem scope
+'''
+Each line is a history
+Each element in the history is incremented to the next one by a certain difference value
+This gives a new list of values
+Each element in the is incremented to the next one by a certain difference value
+When all diferences are 0 the pattern stops and the next value of teh original sequence can be inferred
+The next value is the sum of all the last numbers in each of the value lists
+'''
+
+# Solution sketch
+'''
+Read lines one by one
+For each line create a difference list and append it to a super list
+If all lements of the last list in super list = 0
+...then sum all final elements of all the lists
+... otherwise create a new difference list base on the last list and add it to the superlist
+'''
+
+with open("puzzle_input.txt") as file:
+    input = file.read().splitlines()
+
+first_line_list = [int(x) for x in input[0].split(" ")]
+
+
+# Main code
+def main():
+    superlist = []
+    superlist.append(first_line_list)
+    while sum(superlist[-1]) != 0:
+        new_difference_list = [superlist[-1][index+1]-superlist[-1][index] for index in range(len(superlist[-1])-1)]
+        superlist.append(new_difference_list)
+    for x in superlist: print(x)
+
+main()
--- 2023/new_project.sh
+++ 2023/new_project.sh
@@ -1,8 +1,18 @@
 #! /usr/bin/bash
 echo "Let's go!"
 
-# Define new directory name
-directory = "09"
-mkdir $directory
-cp solver_template.py $directory/solver.py
-touch $directory/puzzle_input.txt
+parent="${1:-.}"    ## parent directory as 1st arg (default .)
+count="${2:-1}"     ## initial count as 2nd arg    (default 1)
+
+printf -v dname "%02d" "$count"           ## store 5 digit number in dname
+
+while [ -d "$parent/$dname" ]             ## while dir exists
+do
+  ((count++))                             ## increment count
+  printf -v dname "%02d" "$count"         ## store new 5 digit number in dname
+done
+
+printf "creating %s\n" "$parent/$dname"   ## (optional) output dirname
+mkdir -p "$parent/$dname"                 ## create dir
+cp solver_template.py $dname/solver.py
+touch $dname/puzzle_input.txt
