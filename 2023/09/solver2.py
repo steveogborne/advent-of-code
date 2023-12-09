9249eeb10c9f77879580f0c345b785248da213f9
Steve Ogborne <steve.ogborne@gmail.com>
2023-12-09 22:44:20 +0000

    Solver2 created. Part scope added, solution changes added. Should be very simple to predict prior history value by modifying maths from sum to minus (almost). Then the sums should be very similat except for the indexes.

---
+++ 2023/09/solver2.py
@@ -0,0 +1,48 @@
+# Problem scope
+'''
+Each line is a history
+Each element in the history is incremented to the next one by a certain difference value
+This gives a new list of values
+Each element in the is incremented to the next one by a certain difference value
+When all diferences are 0 the pattern stops and the next value of teh original sequence can be inferred
+The next value is the sum of all the last numbers in each of the value lists
+
+Part 2: now do the same but predicteding the PREVIOUS history value
+'''
+
+# Solution sketch
+'''
+Read lines one by one
+For each line create a difference list and append it to a super list
+If all lements of the last list in super list = 0
+...then sum all final elements of all the lists
+... otherwise create a new difference list base on the last list and add it to the superlist
+
+PART 2: the difference is that rather than summing you take the first value and minus all the first differences
+Very little code should change
+'''
+
+with open("puzzle_input.txt") as file:
+    input = file.read().splitlines()
+
+history_list = [[int(x) for x in line.split(" ")] for line in input]
+first_history = history_list[0]
+
+
+# Main code
+def main():
+    predictions_sum = 0
+    for history in history_list:
+        superlist = []
+        superlist.append(history)
+        while superlist[-1] != [0 for x in superlist[-1]]:
+            new_difference_list = [superlist[-1][index+1]-superlist[-1][index] for index in range(len(superlist[-1])-1)]
+            superlist.append(new_difference_list)
+        predicted_value = 0
+        for list in superlist:
+            predicted_value += list[-1]
+        print([x[-1] for x in superlist], predicted_value)
+        predictions_sum += predicted_value
+    print("Answer:",predictions_sum)
+
+main()
