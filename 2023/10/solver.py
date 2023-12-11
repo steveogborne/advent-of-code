8711949b60804744e4b253e33bcf6927e0bd9d9b
Steve Ogborne <steve.ogborne@gmail.com>
2023-12-11 18:48:03 +0000

    Created puzzle input and solver for day 10. Summarised the problem and sketched out a solution. Here we go...

--- 2023/10/solver.py
+++ 2023/10/solver.py
@@ -1,17 +1,34 @@
 # Problem scope
 '''
-
+Input is a 2D image. Each pixel is a pipe segment or background.
+The image contains connected loops of pipes and also unconnected pieces and background.
+Pipes are straight pieces: | and -, and bends: J F, 7, L
+There is also a squirrel in one of the loops marked by S
+Find the loop the squirrel is in and find the farthest point in the loop from the squirrel.
+Puzzle answer is the number of steps around the loop the farthest opint is
 '''
 
 # Solution sketch
 '''
-
+Input shouldn't need any processing
+Run a search to find the squirrel and return it's coordinates
+Write a function that takes the starting coordinate and finds the two connecting pipes. (Do manually?)
+Write a function that takes a current location, a previous location and calculates the next position based on the symbol
+Write a function that steps around the loop keeping track of location and steps take
+Write a function that runs both stepping functions in opposite directions simoultaneously until they meet at teh same coordinate.
+Return the steps taken as teh answer.
 '''
 
-# Functions
-
-
 # Variables
+with open("puzzle_input.txt") as file:
+    input = file.read().splitlines()
+
+
+
+# Functions
+def find_start(input):
+    # scan for S retrun coordinates
+
 
 
 # Main code
