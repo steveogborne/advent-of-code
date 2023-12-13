ccd390e209334457dbe574ba1da149187ce4d09f
Steve Ogborne <steve.ogborne@gmail.com>
2023-12-13 12:32:46 +0000

    Started day 11 puzzle. Added problem scope and sketched a solution

--- 2023/11/solver.py
+++ 2023/11/solver.py
@@ -1,11 +1,20 @@
 # Problem scope
 '''
-
+Input is a map of the sky: . is space and # is galaxy
+Need to find sum of shortest distance between every pair
+Pair distance = sum of index differences in x and y direction
+But space has expanded
+Every empty row and column needs to double
 '''
 
 # Solution sketch
 '''
-
+Scan for empty rows and columns, record their indexes
+Insert new empty rows and columns at every empty row and column
+Produce a list of every galaxy coordinate
+For every entry in the list, sum the distance to every other entry in THE REST OF the list
+IN THE REST OF to avoid counting pairs twice
+Sum of sums should be the answer
 '''
 # Variables
 with open("puzzle_input.txt") as file:
