9c1a6cbb201d0c8415ed99547bf20ad820cd1539
Steve Ogborne <steve.ogborne@gmail.com>
2023-12-13 10:11:01 +0000

    Didn't commit prgress because I was trying to do this quickly... WEll I didn't do it that quickly so should be more diligent with commits next time I'm 'racing'. Anyway, wrote a function to sprlit out the input into individual patterns, wrote a function to check for mirror lines in teh patterns, added code to check tha the mirror line is valid, changed the code to handle multiple mirror lines per pattter, wrote a function to transpose patterns to test for vertical mirrors and wrote the final code to iterate over all patterns and return a score

--- 2023/13/solver.py
+++ 2023/13/solver.py
@@ -10,22 +10,89 @@ Sum all scores to get the result
 
 # Solution sketch
 '''
-Process input into lists of patterns
+Process input into lists of patterns /
 Write a function that scans lines to check equivalency. Once equivalency is found check that equivalency matches other lines above and below
 Write a way to do the above with a transposed pattern
 Write the score tracker
 '''
 # Variables
 with open("puzzle_input.txt") as file:
-    input = file.read().splitlines()
+    raw_patterns = file.read().split("\n\n")
+    patterns = [pattern.splitlines() for pattern in raw_patterns]
+    # for line in patterns[0]: print(line)
 
 # Functions
+def find_mirror(pattern, debug_on):
 
+    # Find potential mirror
+    mirror_indexes = []
+    for index, line in enumerate(pattern):
+        #print("check line",index) # test
+        if index > 0 and line == pattern[index-1]:
+            #print("potential mirror found at", index) # test
+            # If potential mirror: check mirror
+            check_index_L = index-2
+            check_index_R = index+1
+            check_limit = len(pattern)-1
+            mirror_check = True
+            while check_index_L >=0 and check_index_R <= check_limit:
+                #print("Check pair:") # test
+                #print(pattern[check_index_L]) # test
+                #print(pattern[check_index_R]) # test
+                if pattern[check_index_L]!=pattern[check_index_R]:
+                    mirror_check = False
+                    if debug_on: # test
+                        print("\nFalse mirror found at:",index, "on lines", check_index_L)
+                        print(pattern[check_index_L], "and", check_index_R)
+                        print(pattern[check_index_R])
+                    break
+                check_index_L-=1
+                check_index_R+=1
+            if mirror_check:
+                mirror_indexes.append(index)
+                if debug_on: # test
+                    print("\nMirror found at:", index)
 
+    return(mirror_indexes)
+
+def transpose_pattern(pattern):
+    pattern_t = [col for col in pattern[0]] # initiate all lines in transposed pattern
+    # print(pattern_t)
+    for index_l, line in enumerate(pattern):
+        if index_l > 0: # don't repeat the first line
+            for index_c, char in enumerate(line):
+                pattern_t[index_c] +=char
+    return(pattern_t)
+
+def score_pattern(pattern, debug_on):
+    if debug_on:
+        print("\nChecking the following pattern for horizintal mirrors:") # test
+        for line in pattern: print(line) # test
+    h_score = find_mirror(pattern, debug_on)
+    h_score = [100*x for x in h_score]
+    if debug_on:
+        print("\nThis pattern scores:", h_score)
+        if len(h_score)>1: print("multi mirror here")
+
+    pattern_t = transpose_pattern(pattern)
+    if debug_on:
+        print("\nChecking the transpose pattern for 'vertical' mirrors:") # test
+        for line in pattern_t: print(line) # test
+    v_score = find_mirror(pattern_t, debug_on)
+    if debug_on:
+        print("\nThis pattern scores:", v_score)
+    if len(h_score)>1: print("multi mirror here!")
+    return(sum(h_score)+sum(v_score))
 
 # Main code
 def main():
-    answer = "Undefined"
+    total_score = 0
+    debug_on = False
+    for index, pattern in enumerate(patterns):
+        if True: # index >= 10 and index <=15:
+            pattern_score = score_pattern(pattern, debug_on)
+            total_score += pattern_score
+    answer = total_score
     print("The solution is:",answer)
 
 main()
