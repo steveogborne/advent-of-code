9c40b02007749c05be126ded18917348d72b2b30
Steve Ogborne <steve.ogborne@gmail.com>
2023-12-14 14:16:03 +0000

    Wrote a function that inputs the file into matrix form. Wrote a function that shifts all rocks north in a given matrix of rocks and cubes. Wrote a function to output the matrix into an image text and checked it looks correct.

--- 2023/14/solver.py
+++ 2023/14/solver.py
@@ -17,14 +17,41 @@ Need a function that calculates weight. Weight is a function of line index and e
 # Variables
 with open("puzzle_input.txt") as file:
     input = file.read().splitlines()
+    platform = [[x for x in l] for l in input]
 
 # Functions
+def shift_rocks(image, direction):
+    if direction == "N":
+        for l_index, line in enumerate(image):
+            for c_index, char in enumerate(line):
+                if char == "O":
+                    search_char=0
+                    if l_index > 0: search_char=image[l_index -1][c_index]
+                    shift_index = 0
+                    # print("rock found")
+                    while search_char == "." and l_index - shift_index > 0:
+                        # print("looking for a space...", shift_index)
+                        shift_index += 1
+                        search_char = image[l_index-1-shift_index][c_index]
+                    # update image O -> . and .-> O
+                    if shift_index > 0:
+                        image[l_index][c_index] = "."
+                        image[l_index - shift_index][c_index] = "O"
+    return(image)
 
+def output_platform_image(image_matrix):
+        image_str = ["".join(line) for line in image_matrix]
+        with open("platform_output.txt", "w") as file2:
+            for line in image_str:
+                file2.write(line+"\n")
 
 
 # Main code
 def main():
-    answer = "Undefined"
+    shifted_platform = shift_rocks(platform, "N")
+    output_platform_image(shifted_platform)
+
+    answer = "in the output image for inspection"
     print("The solution is:",answer)
 
 main()
