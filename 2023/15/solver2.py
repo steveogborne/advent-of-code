a98268e718ac1ed90524bd84bbcf5e7517b2f286
Steve Ogborne <steve.ogborne@gmail.com>
2023-12-15 17:15:33 +0000

    Pretty straight forward part 1. CReated the HASH algorithm and loop. Added file for part 2 and alalysed the problem and solution

--- 2023/15/solver2.py
+++ 2023/15/solver2.py
@@ -9,6 +9,20 @@ Determine the ASCII code for the current character of the string.
 Increase the current value by the ASCII code you just determined.
 Set the current value to itself multiplied by 17.
 Set the current value to the remainder of dividing itself by 256.
+
+Part 2:
+There are 256 boxes that can recieve a number of lenses each. Each lens has focus strength 1-9
+The instructions are split into two parts
+The first part is the lens label (character string)
+The HASH of the label is the destination box
+The second part is an operation and an optional lens focus number
+Operation signified by "-" means remove the lens if it exists from the box
+Operation "=" means:
+> if a lens with this label exists, swap it for the new lens
+> if a lens with this label does not exist then add it to the back of the list
+Running all instrucitons:
+Add up total focusing power of all lenses for the answer
+Focusing power for each lens = (box index +1) * slot index * lens focal length
 '''
 
 # Solution sketch
@@ -21,6 +35,16 @@ Convert to ASCII (.encode('ascii'))
 Maths
 Sum
 Repeat
+
+Part 2:
+Create a list of boxes where each box is a list of lenses, where each lens is a tuple label and a focus:
+[boxes...
+    [lenses...
+        [lens label 0, focus #], [lens label 1, focus #]...
+    ]
+]
+Fill the boxes using the algorithm
+Iterate over all lenses with the maths provided
 '''
 # Variables
 with open("puzzle_input.txt") as file:
