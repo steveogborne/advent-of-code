fa1f5a66a8b48db2c3ce374668a7ab1835ad7541
Steve Ogborne <steve.ogborne@gmail.com>
2023-12-22 23:47:11 +0000

    Wrote the code to iterate over the line maps and calculate the weights. Worked first time phew! That took way too long.

--- 2023/18/solver2.py
+++ 2023/18/solver2.py
@@ -32,7 +32,7 @@ Sort vertices by line
 Iterate over canvas height, interpolating fill from vertices
 '''
 # Variables
-with open("test_input.txt") as file:
+with open("puzzle_input.txt") as file:
     input = file.read().splitlines()
     instructions = []
     for line in input:
@@ -77,7 +77,7 @@ def establish_canvas():
     width = max_r - max_l
     height = max_d - max_u
     start_pos = [0 - max_u, 0 - max_l]
-    print(width, height, start_pos,"\n")
+    #print(width, height, start_pos,"\n")
 
     # Adjust all vertices such that canvas origin is at 0-index
     vertices = [[vertex[0]-max_u, vertex[1]-max_l] for vertex in vertices]
@@ -96,91 +96,92 @@ def establish_canvas():
     # Check to see if we need branching logic (highly likely!)
     # ...and whether we need neighbour line exceptions (hopefully not!)
     # previous_line_index = 0
-    for line in line_vertices:
-        print(line)
+    #for line in line_vertices:
+        #print(line)
         # if len(line[1])>2: print("Multiple branches here") # yep, many found
         # if previous_line_index == line[0]-1: print("Neighbour line found") # phew, no neighbours
 
     # For each line turn that list into a start stop map accounting for edges of existing branches
     # target format: line = [row, [col1, col2, ...]]
     line_maps = [line_vertices[0]] # initialise
-    print("\nFirst line in line_maps:",line_maps)
+    #print("\nFirst line in line_maps:",line_maps)
 
     for index, new_vertices in enumerate(line_vertices[1::]):
         # line_maps.append([line[0], line_maps[-1][1]])
         # print("\n"+"line maps[-1]", line_maps[-1])
         # print("line in line_vertices", line)
         old_vs = line_maps[-1][1].copy()
-        print("old vertices", old_vs)
+        #print("old vertices", old_vs)
         extra_list = line_maps[-1][1].copy()
         add_new_line = False
-        print("new vertices", new_vertices)
-        print("new pairs indexes:",list(range(0, len(new_vertices[1])//2)))
+        #print("new vertices", new_vertices)
+        #print("new pairs indexes:",list(range(0, len(new_vertices[1])//2)))
 
         for np in range(0, len(new_vertices[1])//2): # for each actual new pair
             a = new_vertices[1][2*np]
             b = new_vertices[1][2*np+1]
             if a in old_vs and b not in old_vs and old_vs.index(a) %2 == 0:
-                print("index even, a->b (reduce left) (new line)")
+                #print("index even, a->b (reduce left) (new line)")
                 extra_list.append(b)
                 extra_list.remove(a)
                 extra_list.sort()
                 add_new_line = True
-                print("old_vs",old_vs,"extra_line",extra_list)
+                #print("old_vs",old_vs,"extra_line",extra_list)
             elif a in old_vs and b not in old_vs and old_vs.index(a) %2 == 1:
-                print("index odd, a->b (extend right) (same line)")
+                #print("index odd, a->b (extend right) (same line)")
                 old_vs.append(b)
                 old_vs.remove(a)
                 old_vs.sort()
-                print("old_vs",old_vs,"extra_line",extra_list)
+                #print("old_vs",old_vs,"extra_line",extra_list)
             elif b in old_vs and a not in old_vs and old_vs.index(b) %2 == 0:
-                print("index even, b->a (extend left) (same line)")
+                #print("index even, b->a (extend left) (same line)")
                 old_vs.append(a)
                 old_vs.remove(b)
                 old_vs.sort()
-                print("old_vs",old_vs,"extra_line",extra_list)
+                #print("old_vs",old_vs,"extra_line",extra_list)
             elif b in old_vs and a not in old_vs and old_vs.index(b) %2 == 1:
-                print("index odd, b->a (reduce right) (new line)")
+                #print("index odd, b->a (reduce right) (new line)")
                 extra_list.append(a)
                 extra_list.remove(b)
                 extra_list.sort()
                 add_new_line = True
-                print("old_vs",old_vs,"extra_line",extra_list)
+                #print("old_vs",old_vs,"extra_line",extra_list)
             elif a in old_vs and b in old_vs and old_vs.index(a) %2 == 0:
-                print("index even, remove a,b (cap branch) (new line)")
+                #print("index even, remove a,b (cap branch) (new line)")
                 extra_list.remove(a)
                 extra_list.remove(b)
                 extra_list.sort()
                 add_new_line = True
-                print("old_vs",old_vs,"extra_line",extra_list)
+                #print("old_vs",old_vs,"extra_line",extra_list)
             elif a in old_vs and b in old_vs and old_vs.index(a) %2 == 1:
-                print("index odd, remove a,b (join branch) (same line)")
+                #print("index odd, remove a,b (join branch) (same line)")
                 old_vs.remove(a)
                 old_vs.remove(b)
                 old_vs.sort()
-                print("old_vs",old_vs,"extra_line",extra_list)
+                #print("old_vs",old_vs,"extra_line",extra_list)
             elif a not in old_vs and b not in old_vs:
                 old_vs.extend([a,b])
                 old_vs.sort()
-                print(old_vs,"index a =", old_vs.index(a))
+                #print(old_vs,"index a =", old_vs.index(a))
                 if old_vs.index(a) %2 == 0:
-                    print("pair is new branch (same line)")
-                    print("old_vs",old_vs,"extra_line",extra_list)
+                    continue
+                    #print("pair is new branch (same line)")
+                    #print("old_vs",old_vs,"extra_line",extra_list)
                 elif old_vs.index(a) %2 ==1:
-                    print("pair is split branch (new line)")
+                    #print("pair is split branch (new line)")
                     old_vs.remove(a)
                     old_vs.remove(b)
                     old_vs.sort()
                     extra_list.extend([a,b])
                     extra_list.sort()
                     add_new_line = True
-                    print("old_vs",old_vs,"extra_line",extra_list)
+                    #print("old_vs",old_vs,"extra_line",extra_list)
         line_maps.append([new_vertices[0], old_vs])
         if add_new_line: line_maps.append([new_vertices[0]+1, extra_list])
-        print("\nLast line in line_maps:",line_maps[-1])
+        #print("\nLast line in line_maps:",line_maps[-1])
 
     # print("line maps", line_maps)
-    print("\n")
+    #print("\n")
     return line_maps
 
 # Functions
@@ -188,12 +189,25 @@ def establish_canvas():
 # Main code
 def main():
     list = establish_canvas()
+    total_weight = 0
+    line_weight = 0
+    last_line = 0
+    for line in list:
+        line_weight *= line[0] - last_line
+        total_weight += line_weight
+        line_weight = 0
+        for pair in range(0, len(line[1])//2):
+            a = line[1][2*pair]
+            b = line[1][2*pair+1]
+            line_weight += b-a +1
+        last_line = line[0]
+
     # canvas = paint_infill(canvas, start_pos)
     # canvas = output_canvas(canvas, "puzzle_output.txt")
     # area = count_holes(canvas)
-    for line in list: print(line)
+    # for line in list: print(line)
 
-    answer = "Undefined"
+    answer = total_weight
     print("The solution is:",answer)
 
 main()
