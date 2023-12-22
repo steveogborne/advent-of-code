0c155fd202cdb07b99b7b3c510de71f0f7be3bc3
Steve Ogborne <steve.ogborne@gmail.com>
2023-12-22 23:32:31 +0000

    It took a few failed iterations but I implemented the logic tree for turning each line of vertices into a start, stop map for solid sections

--- 2023/18/solver2.py
+++ 2023/18/solver2.py
@@ -44,102 +44,155 @@ with open("test_input.txt") as file:
             case 3: direction = "U"
         distance = int(line.split(" ")[2][2:-2:],16)
         instructions.append([direction, distance])
-    for line in instructions: print(line)
+    # for line in instructions: print(line)
 
 def establish_canvas():
-    canvas_u = 0
-    canvas_l = 0
-    canvas_r = 0
-    canvas_d = 0
+    # collect vertices while tracing instructions
+    max_u = 0
+    max_l = 0
+    max_r = 0
+    max_d = 0
     pos = [0,0]
-    canvas = [[0,0]]
+    vertices = [] #  collect outline vertices here
     for line in instructions:
         match line[0]:
             case "U":
-                pos[1] -= line[1] # indexes are +ve downwards!
-                canvas.append([pos[0],pos[1]])
-                if pos[1] < canvas_u: canvas_u = pos[1]
+                pos[0] -= line[1] # indexes are +ve downwards!
+                vertices.append([pos[0],pos[1]])
+                if pos[0] < max_u: max_u = pos[0]
             case "L":
-                pos[0] -= line[1]
-                canvas.append([pos[0],pos[1]])
-                if pos[0] < canvas_l: canvas_l = pos[0]
+                pos[1] -= line[1]
+                vertices.append([pos[0],pos[1]])
+                if pos[1] < max_l: max_l = pos[1]
             case "R":
-                pos[0] += line[1]
-                canvas.append([pos[0],pos[1]])
-                if pos[0] > canvas_r: canvas_r = pos[0]
+                pos[1] += line[1]
+                vertices.append([pos[0],pos[1]])
+                if pos[1] > max_r: max_r = pos[1]
             case "D":
-                pos[1] += line[1] # indexes are +ve downwards!
-                canvas.append([pos[0],pos[1]])
-                if pos[1] > canvas_d: canvas_d = pos[1]
-    # print(pos) # check return to start
-    # print(canvas_u, canvas_l, canvas_r, canvas_d) # observe max deltas from start in all directions
-    width = canvas_r - canvas_l
-    height = canvas_d - canvas_u
-    start_pos = [0 - canvas_u, 0 - canvas_l] # set start point such that canvas origin is at 0-index
-    print(width, height, start_pos)
-    for vertice in canvas: print(vertice)
-    return canvas, start_pos
+                pos[0] += line[1] # indexes are +ve downwards!
+                vertices.append([pos[0],pos[1]])
+                if pos[0] > max_d: max_d = pos[0]
+
+    # canvas stats
+    width = max_r - max_l
+    height = max_d - max_u
+    start_pos = [0 - max_u, 0 - max_l]
+    print(width, height, start_pos,"\n")
+
+    # Adjust all vertices such that canvas origin is at 0-index
+    vertices = [[vertex[0]-max_u, vertex[1]-max_l] for vertex in vertices]
+    vertices.sort() # sort vertices from top to bottom for scanning
+
+    # Put all vertices of the same line into one list
+    line_vertices = []
+    current_line = -1
+    for vertex in vertices:
+        if vertex[0] == current_line:
+            line_vertices[-1][-1].append(vertex[1])
+        if vertex[0] != current_line:
+            line_vertices.append([vertex[0], [vertex[1]]])
+            current_line = vertex[0]
+
+    # Check to see if we need branching logic (highly likely!)
+    # ...and whether we need neighbour line exceptions (hopefully not!)
+    # previous_line_index = 0
+    for line in line_vertices:
+        print(line)
+        # if len(line[1])>2: print("Multiple branches here") # yep, many found
+        # if previous_line_index == line[0]-1: print("Neighbour line found") # phew, no neighbours
+
+    # For each line turn that list into a start stop map accounting for edges of existing branches
+    # target format: line = [row, [col1, col2, ...]]
+    line_maps = [line_vertices[0]] # initialise
+    print("\nFirst line in line_maps:",line_maps)
+
+    for index, new_vertices in enumerate(line_vertices[1::]):
+        # line_maps.append([line[0], line_maps[-1][1]])
+        # print("\n"+"line maps[-1]", line_maps[-1])
+        # print("line in line_vertices", line)
+        old_vs = line_maps[-1][1].copy()
+        print("old vertices", old_vs)
+        extra_list = line_maps[-1][1].copy()
+        add_new_line = False
+        print("new vertices", new_vertices)
+        print("new pairs indexes:",list(range(0, len(new_vertices[1])//2)))
+
+        for np in range(0, len(new_vertices[1])//2): # for each actual new pair
+            a = new_vertices[1][2*np]
+            b = new_vertices[1][2*np+1]
+            if a in old_vs and b not in old_vs and old_vs.index(a) %2 == 0:
+                print("index even, a->b (reduce left) (new line)")
+                extra_list.append(b)
+                extra_list.remove(a)
+                extra_list.sort()
+                add_new_line = True
+                print("old_vs",old_vs,"extra_line",extra_list)
+            elif a in old_vs and b not in old_vs and old_vs.index(a) %2 == 1:
+                print("index odd, a->b (extend right) (same line)")
+                old_vs.append(b)
+                old_vs.remove(a)
+                old_vs.sort()
+                print("old_vs",old_vs,"extra_line",extra_list)
+            elif b in old_vs and a not in old_vs and old_vs.index(b) %2 == 0:
+                print("index even, b->a (extend left) (same line)")
+                old_vs.append(a)
+                old_vs.remove(b)
+                old_vs.sort()
+                print("old_vs",old_vs,"extra_line",extra_list)
+            elif b in old_vs and a not in old_vs and old_vs.index(b) %2 == 1:
+                print("index odd, b->a (reduce right) (new line)")
+                extra_list.append(a)
+                extra_list.remove(b)
+                extra_list.sort()
+                add_new_line = True
+                print("old_vs",old_vs,"extra_line",extra_list)
+            elif a in old_vs and b in old_vs and old_vs.index(a) %2 == 0:
+                print("index even, remove a,b (cap branch) (new line)")
+                extra_list.remove(a)
+                extra_list.remove(b)
+                extra_list.sort()
+                add_new_line = True
+                print("old_vs",old_vs,"extra_line",extra_list)
+            elif a in old_vs and b in old_vs and old_vs.index(a) %2 == 1:
+                print("index odd, remove a,b (join branch) (same line)")
+                old_vs.remove(a)
+                old_vs.remove(b)
+                old_vs.sort()
+                print("old_vs",old_vs,"extra_line",extra_list)
+            elif a not in old_vs and b not in old_vs:
+                old_vs.extend([a,b])
+                old_vs.sort()
+                print(old_vs,"index a =", old_vs.index(a))
+                if old_vs.index(a) %2 == 0:
+                    print("pair is new branch (same line)")
+                    print("old_vs",old_vs,"extra_line",extra_list)
+                elif old_vs.index(a) %2 ==1:
+                    print("pair is split branch (new line)")
+                    old_vs.remove(a)
+                    old_vs.remove(b)
+                    old_vs.sort()
+                    extra_list.extend([a,b])
+                    extra_list.sort()
+                    add_new_line = True
+                    print("old_vs",old_vs,"extra_line",extra_list)
+        line_maps.append([new_vertices[0], old_vs])
+        if add_new_line: line_maps.append([new_vertices[0]+1, extra_list])
+        print("\nLast line in line_maps:",line_maps[-1])
+
+    # print("line maps", line_maps)
+    print("\n")
+    return line_maps
 
 # Functions
-def paint_outline(canvas, start_pos):
-    pos = start_pos
-    for line in instructions:
-        count = 0
-        canvas[pos[0]][pos[1]] = "#"
-        while count < line[1]:
-            match line[0]:
-                case "U": pos[0] -=1
-                case "L": pos[1] -=1
-                case "R": pos[1] +=1
-                case "D": pos[0] +=1
-            canvas[pos[0]][pos[1]] = "#"
-            # print(pos)
-            count +=1
-    return canvas
-
-def paint_infill(canvas, start_pos):
-    # Visual inspection of test_input and puzzle_input shows that the instructions travel CW around the outline
-    # Therefore inside is to the right of up
-    pos = start_pos
-    for line in instructions:
-            count = 0
-            while count < line[1]:
-                match line[0]:
-                    case "U":
-                        fill_pos = [pos[0],pos[1]+1]
-                        while canvas[fill_pos[0]][fill_pos[1]] != "#":
-                            canvas[fill_pos[0]][fill_pos[1]] = "#"
-                            fill_pos[1] +=1
-                        pos[0] -=1
-                        fill_pos = [pos[0],pos[1]+1]
-                        while canvas[fill_pos[0]][fill_pos[1]] != "#":
-                            canvas[fill_pos[0]][fill_pos[1]] = "#"
-                            fill_pos[1] +=1
-                    case "L": pos[1] -=1
-                    case "R": pos[1] +=1
-                    case "D": pos[0] +=1
-                # print(pos)
-                count +=1
-    return canvas
-
-def output_canvas(canvas, dir):
-        with open(dir, "w") as file2:
-            for line in canvas:
-                file2.write("".join(line)+"\n")
-
-def count_holes(canvas):
-    count = 0
-    for line in canvas:
-        count += line.count("#")
-    return count
 
 # Main code
 def main():
-    canvas, start_pos = establish_canvas()
-    # canvas = paint_outline(canvas, start_pos)
+    list = establish_canvas()
     # canvas = paint_infill(canvas, start_pos)
     # canvas = output_canvas(canvas, "puzzle_output.txt")
     # area = count_holes(canvas)
+    for line in list: print(line)
+
     answer = "Undefined"
     print("The solution is:",answer)
 
