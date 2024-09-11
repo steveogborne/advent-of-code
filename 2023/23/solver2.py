05ebc6bff966184f871d1f10a30edd6d588d94d0
Steve Ogborne <steve.ogborne@gmail.com>
2024-09-11 05:26:34 +0100

    Day 23 Part 2. Slopes now irrelevant so directed graph now undirected. Updated algo to keep track of nodes travelled to stop double stepping. Brute force search works with example but obvs not with larger network so need to find a way to simplify problem

--- 2023/23/solver2.py
+++ 2023/23/solver2.py
@@ -6,6 +6,10 @@ from collections import defaultdict
 Walk through the maze in the woods.
 ^,<,>,v are slopes. You can only go down slopes
 What is the longest route through the woods?
+
+Part 2:
+Slopes don't matter anymore
+What is the longest non repeating route?
 '''
 
 # Solution sketch
@@ -15,15 +19,22 @@ Need a tracker algorithm that builds a network of the woods
 The network can have nodes which are the fork points
 The edges will have distances between nodes
 Then need a longest path. BFScl
+
+Part 2:
+Nodes and edges remain the same but edges not directed
+Can keep the network establishment the same
+Need to fix the edges so that I can reference the start node
+Add a edge method to traverse it given either start or end node
+Update route finders to keep track of nodes or edges traversed for each route to avoid double stepping
+I think distances should work out.
+Need to choose from inbound and outbound edges to traverse
 '''
 # Variables
 with open("puzzle_input.txt") as file:
     grid = file.read().splitlines()
 
-# grid[0] = grid[0][0] + "v" + grid[0][2:] # Simplify walk algorithm
-
 final = (len(grid)-1, len(grid[0])-2)
-print("Final:",final)
+# print("Final:",final)
 
 # Functions
 Position: TypeAlias = tuple[int, int]
@@ -38,21 +49,25 @@ class Node():
         return f"{n.pos}, in: {len(n.inb)}, out: {len(n.outb)}"
 
 class Edge():
-    def __init__(e, start: Position, end: Node = None, l: int = None) -> None:
+    def __init__(e, dir: str, start: Node, end: Node = None, l: int = None) -> None:
+        e.dir = dir # Direction edge travels to initiate edge
         e.start = start # Stores reference to start node
         e.end = end # Stores reference to end node
         e.l = l # Stores length of
 
     def __str__(e) -> str:
         en = e.end.pos if e.end else "TBC"
-        return f"{e.start} -({e.l})-> {en}"
+        return f"{e.start.pos} -({e.l})-> {en}"
 
     def walk(e, grid):
         stopped = False
         # print("Start:", e.start)
-        r,c = e.start
-        dist = 1 if grid[r][c] != "." else 0
-        last = grid[r][c] if grid[r][c] != "." else "v"
+        r,c = e.start.pos
+        last = e.dir
+        match last:
+            case ">": c += 1
+            case "v": r += 1
+        dist = 1
 
         while not stopped:
             east = south = west = north = None
@@ -95,6 +110,10 @@ class Edge():
         e.l = dist
         return endpos
 
+    def traverseFrom(e, start) -> Node:
+        if e.start == start: return e.end
+        else: return e.start
+
 # Main code
 def main():
     ### Map Network
@@ -105,7 +124,7 @@ def main():
     # Initialise start node and first edge
     startpos = (0,1)
     start_n = Node(startpos)
-    start_e = Edge(startpos)
+    start_e = Edge("v", start_n)
     start_n.outb.append(start_e)
     nodes[startpos] = start_n
 
@@ -126,13 +145,13 @@ def main():
             try: east = grid[endpos[0]][endpos[1]+1]
             except: east = None
             if east == ">":
-                east_e = Edge((endpos[0],endpos[1]+1))
+                east_e = Edge(">", nodes[endpos])
                 frontiers.append(east_e)
                 nodes[endpos].outb.append(east_e)
             try: south = grid[endpos[0]+1][endpos[1]]
             except: south = None
             if south == "v":
-                south_e = Edge((endpos[0]+1,endpos[1]))
+                south_e = Edge("v", nodes[endpos])
                 frontiers.append(south_e)
                 nodes[endpos].outb.append(south_e)
 
@@ -146,29 +165,38 @@ def main():
 
     print("\nNodes")
     for node in nodes.values(): print(node)
+    edges.sort(key = lambda x: x.start.pos)
     print("\nEdges")
     for edge in edges: print(edge)
 
     ### Longest path
     # Keep a list of route frontiers, initialise with start node
-    routes = [(nodes[(0,1)], 0)]
+    routes = [(nodes[(0,1)], 0, [])]
     route_ds = []
+    count = 0
 
     # While end node not found:
-    while len(routes) > 0:
+    while len(routes) > 0 and count < 1000:
+        count += 1
         # Traverse to next node, create new frontiers based on number of departing edges
-        current_n, current_d = routes.pop(0)
-        if current_n.outb:
-            new_es = current_n.outb
-            for e in new_es:
-                routes.append((e.end, current_d + e.l))
+        current_n, current_d, comp_n = routes.pop()
+        if current_n.pos != final:
+            es = current_n.outb + current_n.inb
+            ns = [(e.traverseFrom(current_n), e.l) for e in es]
+            valid_ns = [(n, d) for (n, d) in ns if n not in comp_n]
+            # print("Valid nodes:", *valid_ns)
+            for (n, d) in valid_ns:
+                routes.append((n, current_d + d, comp_n+[current_n]))
         else:
             route_ds.append(current_d)
+            # print(current_d, current_n, len(comp_n))
 
+    print("\nRoutes in progress")
+    for route in routes[0:10]: print(*route[2], route[0], route[1])
     print("\nRoute distances:")
     print(route_ds)
 
-    answer = max(route_ds)
+    answer = max(route_ds) if len(route_ds) > 0 else "TBC"
     print("The solution is:",answer)
 
 main()
